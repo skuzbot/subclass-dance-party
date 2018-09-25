c273ae5d2f00395bd953871696deae4427621f5e
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-25 00:42:51 -0500

    Pass Specs

--- src/twirlyDancer.js
+++ src/twirlyDancer.js
@@ -7,10 +7,6 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-
-// we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-// so we must keep a copy of the old version of this function
-
 BlinkyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
   this.$node.toggle();
